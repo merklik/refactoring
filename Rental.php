063a5cf56c9514a7bbb21d675ba47bde0885a119
merklik <merklik@gmail.com>
2013-04-27 23:36:14 +0200

    getCharged extraxted and moved, thisAmount removed

--- Rental.php
+++ Rental.php
@@ -10,19 +10,50 @@
 namespace Refactoring;
 
 
-class Rental {
+class Rental
+{
 
-private  $_movie;
-private $_daysRented;
+    private $_movie;
+    private $_daysRented;
 
-function __construct($movie, $daysRented) {
-    $this->_movie = $movie;
-    $this->_daysRented = $daysRented;
-}
-public function getDaysRented() {
-    return $this->_daysRented;
-}
-public function getMovie() {
-    return $this->_movie;
-}
+    function __construct($movie, $daysRented)
+    {
+        $this->_movie = $movie;
+        $this->_daysRented = $daysRented;
+    }
+
+    public function getDaysRented()
+    {
+        return $this->_daysRented;
+    }
+
+    public function getMovie()
+    {
+        return $this->_movie;
+    }
+
+
+    public function getCharge()
+    {
+        $result = 0;
+
+        switch ($this->getMovie()->getPriceCode()) {
+            case Movie::REGULAR:
+                $result += 2;
+                if ($this->getDaysRented() > 2)
+                    $result += ($this->getDaysRented() - 2) * 1.5;
+                break;
+            case Movie::NEW_RELEASE:
+                $result += $this->getDaysRented() * 3;
+                break;
+            case Movie::CHILDRENS:
+                $result += 1.5;
+                if ($this->getDaysRented() > 3)
+                    $result += ($this->getDaysRented() - 3) * 1.5;
+                break;
+
+        }
+        return $result;
+    }
+
 }
