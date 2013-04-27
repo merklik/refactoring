6d93c4fbabb45e740ac3370f7da2ce8304da7d84
merklik <merklik@gmail.com>
2013-04-28 00:24:55 +0200

    Price is introduced

--- Rental.php
+++ Rental.php
@@ -37,38 +37,13 @@ class Rental
      */
     public function getFrequentRenterPoints()
     {
-        // add bonus for a two day new release rental
-        if (($this->getMovie()->getPriceCode() == Movie::NEW_RELEASE)
-            &&
-            $this->getDaysRented() > 1
-        ){
-            return 2;
-        }
-        return 1;
+        return $this->_movie->getFrequentRenterPoints($this->_daysRented);
     }
 
 
     public function getCharge()
     {
-        $result = 0;
-
-        switch ($this->getMovie()->getPriceCode()) {
-            case Movie::REGULAR:
-                $result += 2;
-                if ($this->getDaysRented() > 2)
-                    $result += ($this->getDaysRented() - 2) * 1.5;
-                break;
-            case Movie::NEW_RELEASE:
-                $result += $this->getDaysRented() * 3;
-                break;
-            case Movie::CHILDRENS:
-                $result += 1.5;
-                if ($this->getDaysRented() > 3)
-                    $result += ($this->getDaysRented() - 3) * 1.5;
-                break;
-
-        }
-        return $result;
+        return $this->_movie->getCharge($this->_daysRented);
     }
 
 }
